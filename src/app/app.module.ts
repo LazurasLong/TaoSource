fa4de4204459def7dd80b74575d243dcba5386e5
OdicforceSounds <om@odicforcesounds.com>
2017-10-20 14:47:48 +0100

    organized directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,22 +10,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LicenseComponent } from './pages/license/license.component';
 import { WhoComponent } from './pages/who/who.component';
-import { SoundcloudComponent } from './pages/soundcloud/soundcloud.component';
-import { MixcloudComponent } from './pages/mixcloud/mixcloud.component';
+// import { SoundcloudComponent } from './pages/soundcloud/soundcloud.component';
+// import { MixcloudComponent } from './pages/mixcloud/mixcloud.component';
 import { BandcampComponent } from './pages/bandcamp/bandcamp.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
 import { WikiComponent } from './pages/wiki/wiki.component';
 import { LoginComponent } from './pages/login/login.component';
-import { VotComponent } from './pages/articles/vot/vot.component';
-import { MwComponent } from './pages/articles/mw/mw.component';
-import { StComponent } from './pages/articles/st/st.component';
-import { SkdComponent } from './pages/articles/skd/skd.component';
-import { EtComponent } from './pages/articles/et/et.component';
-import { NfiComponent } from './pages/articles/nfi/nfi.component';
-import { EsComponent } from './pages/articles/es/es.component';
-import { OtComponent } from './pages/articles/ot/ot.component';
-import { WwccComponent } from './pages/articles/more/wwcc/wwcc.component';
-import { WwbcComponent } from './pages/articles/more/wwbc/wwbc.component';
+import { VotComponent } from './pages/bandcamp/albums/vot/vot.component';
+import { MwComponent } from './pages/bandcamp/albums/mw/mw.component';
+import { StComponent } from './pages/bandcamp/albums/st/st.component';
+import { SkdComponent } from './pages/bandcamp/albums/skd/skd.component';
+import { EtComponent } from './pages/bandcamp/albums/et/et.component';
+import { NfiComponent } from './pages/bandcamp/albums/nfi/nfi.component';
+import { EsComponent } from './pages/bandcamp/albums/es/es.component';
+import { OtComponent } from './pages/bandcamp/albums/ot/ot.component';
+// import { WwccComponent } from './pages/articles/more/wwcc/wwcc.component';
+// import { WwbcComponent } from './pages/articles/more/wwbc/wwbc.component';
 import { WhoweareComponent } from './pages/articles/more/whoweare/whoweare.component';
 import { UtellmeComponent } from './pages/articles/more/utellme/utellme.component';
 import { IntroComponent } from './pages/wiki/intro/intro.component';
@@ -34,7 +34,7 @@ import { CoreconceptsComponent } from './pages/wiki/coreconcepts/coreconcepts.co
 import { IdeaComponent } from './pages/wiki/idea/idea.component';
 import { ChallengeComponent } from './pages/wiki/challenge/challenge.component';
 import { PathComponent } from './pages/wiki/path/path.component';
-import { PrivacyComponent } from './pages/wiki/privacy/privacy.component';
+// import { PrivacyComponent } from './pages/wiki/privacy/privacy.component';
 
 
 const appRoutes: Routes = [
@@ -42,23 +42,23 @@ const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'who', component: WhoComponent },
   { path: 'license', component: LicenseComponent },
-  { path: 'mixcloud', component: MixcloudComponent },
-  { path: 'soundcloud', component: SoundcloudComponent },
+//  { path: 'mixcloud', component: MixcloudComponent },
+//  { path: 'soundcloud', component: SoundcloudComponent },
   { path: 'bandcamp', component: BandcampComponent },
   { path: 'articles', component: ArticlesComponent },
   { path: 'login', component: LoginComponent },
   { path: 'wiki', component: WikiComponent },
-  { path: 'articles/vot', component: VotComponent },
-  { path: 'articles/mw', component: MwComponent },
-  { path: 'articles/st', component: StComponent },
-  { path: 'articles/skd', component: SkdComponent },
-  { path: 'articles/et', component: EtComponent },
-  { path: 'articles/nfi', component: NfiComponent },
-  { path: 'articles/es', component: EsComponent },
-  { path: 'articles/ot', component: OtComponent },
+  { path: 'bc/a/vot', component: VotComponent },
+  { path: 'bc/a/mw', component: MwComponent },
+  { path: 'bc/a/st', component: StComponent },
+  { path: 'bc/a/skd', component: SkdComponent },
+  { path: 'bc/a/et', component: EtComponent },
+  { path: 'bc/a/nfi', component: NfiComponent },
+  { path: 'bc/a/es', component: EsComponent },
+  { path: 'bc/a/ot', component: OtComponent },
   { path: 'articles/whoweare', component: WhoweareComponent },
-  { path: 'articles/wwcc', component: WwccComponent },
-  { path: 'articles/wwbc', component: WwbcComponent },
+//  { path: 'articles/wwcc', component: WwccComponent },
+//  { path: 'articles/wwbc', component: WwbcComponent },
   { path: 'articles/more/utellme', component: UtellmeComponent },
   { path: 'wiki/intro', component: IntroComponent },
   { path: 'wiki/firststeps', component: FirststepsComponent },
@@ -66,7 +66,7 @@ const appRoutes: Routes = [
   { path: 'wiki/idea', component: IdeaComponent },
   { path: 'wiki/challenge', component: ChallengeComponent },
   { path: 'wiki/path', component: PathComponent },
-  { path: 'wiki/privacy', component: PrivacyComponent }
+//  { path: 'wiki/privacy', component: PrivacyComponent }
 ]
 
 @NgModule({
@@ -75,8 +75,8 @@ const appRoutes: Routes = [
     HomeComponent,
     LicenseComponent,
     WhoComponent,
-    SoundcloudComponent,
-    MixcloudComponent,
+//    SoundcloudComponent,
+//    MixcloudComponent,
     BandcampComponent,
     ArticlesComponent,
     WikiComponent,
@@ -89,8 +89,8 @@ const appRoutes: Routes = [
     NfiComponent,
     EsComponent,
     OtComponent,
-    WwccComponent,
-    WwbcComponent,
+//    WwccComponent,
+//    WwbcComponent,
     WhoweareComponent,
     UtellmeComponent,
     IntroComponent,
@@ -99,7 +99,7 @@ const appRoutes: Routes = [
     IdeaComponent,
     ChallengeComponent,
     PathComponent,
-    PrivacyComponent
+//    PrivacyComponent
   ],
   imports: [
     BrowserModule,
