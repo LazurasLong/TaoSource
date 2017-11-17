e8a1ab8b52a117f364a85fe32404a07b9207bed1
odicforcesounds <soundsofodicforce@gmail.com>
2017-11-17 01:44:19 +0000

    this script will help me a bit

--- NgBuild.sh
+++ NgBuild.sh
@@ -5,5 +5,9 @@ rm dist/*.gz &&
 echo "gz files removed from dist/" &&
 cp -Rv redirects/* dist/ &&
 echo "dist/ folder ready to firebase deploy" &&
-git add . &&
-echo "changes saved in git - give your own commit message!"
+git add . && 
+firebase deploy &&
+echo "Lets hope there is no error >D - commiting / publishing project" &&
+echo "while we wait for firebase upload...lets give them more 20 secounds " && sleep 20 &&
+echo "changes saved in git - give your own commit message!" &&
+echo "git commit -m"
