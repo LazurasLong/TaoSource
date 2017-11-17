3ae87fc5064c36877fff3a739c7c5e05dd337629
odicforcesounds <soundsofodicforce@gmail.com>
2017-11-17 06:07:05 +0000

    NgBuild.sh: update firebase update git

--- NgBuild.sh
+++ NgBuild.sh
@@ -7,7 +7,6 @@ cp -Rv redirects/* dist/ &&
 echo "dist/ folder ready to firebase deploy" &&
 git add . && 
 echo "Lets hope there is no error >D - commiting / publishing project" &&
-echo "while we wait for firebase upload...lets give them more 60 secounds " && sleep 60 &&
-echo "changes saved in git - give your own commit message!" &&
-echo "git commit -m"
+echo "while we wait for firebase upload...lets give them more 60 secounds " && 
+echo "changes saved in git - give your own commit message!" &&"
 firebase deploy 
