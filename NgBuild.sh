135335690c6b1a399b69b38a8ca902217fd1c8ba
odicforcesounds <soundsofodicforce@gmail.com>
2017-11-17 23:32:02 +0000

    last time? fixed...

--- NgBuild.sh
+++ NgBuild.sh
@@ -8,5 +8,5 @@ echo "dist/ folder ready to firebase deploy" &&
 git add . && 
 echo "Lets hope there is no error >D - commiting / publishing project" &&
 echo "while we wait for firebase upload...lets give them more 60 secounds " && 
-echo "changes saved in git - give your own commit message!" &&"
+echo "changes saved in git - give your own commit message!" &&
 firebase deploy 
