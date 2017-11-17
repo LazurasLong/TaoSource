80f01b369c3fe8d91adb920d23698f13168c326d
odicforcesounds <soundsofodicforce@gmail.com>
2017-11-17 01:33:09 +0000

    lets start a new night of odicforcesounds updates

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,1 +1,9 @@
-ng build --prod --output-hashing --sourcemaps --build-optimizer
+ng build --prod --output-hashing --sourcemaps &&
+sleep 4 &&
+echo "project is built" &&
+rm dist/*.gz &&
+echo "gz files removed from dist/" &&
+cp -Rv redirects/* dist/ &&
+echo "dist/ folder ready to firebase deploy" &&
+git add . &&
+echo "changes saved in git - give your own commit message!"
