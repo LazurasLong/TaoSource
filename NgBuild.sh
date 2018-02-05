88d4d86696ba5c362a77696d941fc29db625227e
odicforcesounds <om@odicforcesounds.com>
2018-02-05 10:52:15 +0000

    delete more crap :D

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,8 +1,6 @@
 ng build --target=production --environment=prod --aot --build-optimizer --vendor-chunk=true --output-hashing --sourcemaps &&
 sleep 40 &&
 echo "project is built" &&
-rm dist/*.gz &&
-echo "gz files removed from dist/" &&
-cp -Rv redirects/* dist/* . &&
+cp -Rv redirects/* dist/* . && rm *.map &&
 git add . && git commit -m "set me alive at github" && git push
 echo "Done - in few secounds / minutes we can see the result alive"
