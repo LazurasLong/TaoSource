3573bba74db0c1f9b651895e38d49f5771bd2c49
odicforcesounds <om@odicforcesounds.com>
2018-02-06 17:41:19 +0000

    set me alive at github

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,6 +1,6 @@
-ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --sourcemaps &&
+ng build --target=production --environment=prod --aot --build-optimizer --output-hashing &&
 sleep 40 &&
 echo "project is built" &&
-cp -Rv redirects/* dist/* . && rm *.map &&
+cp -Rv redirects/* dist/* . && 
 git add . && git commit -m "set me alive at github" && git push
 echo "Done - in few secounds / minutes we can see the result alive"
