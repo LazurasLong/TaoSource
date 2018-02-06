e3324e37e0d682d0ec8ceba88948cbcd5d145436
odicforcesounds <om@odicforcesounds.com>
2018-02-06 22:26:21 +0000

    Github & JavaScript doesn't go good !

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,1 +1,1 @@
-ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path docs --base-href "https://odicforcesounds.github.io/odicforcesounds.com/"
+ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
