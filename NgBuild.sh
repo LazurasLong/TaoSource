9b3f5af722864a3ed5fde575a4927f518ababbff
odicforcesounds <om@odicforcesounds.com>
2018-02-06 18:53:20 +0000

    More Organized ! :D

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,6 +1,2 @@
-ng build --target=production --environment=prod --aot --build-optimizer --output-hashing &&
-sleep 40 &&
-echo "project is built" &&
-cp -Rv redirects/* dist/* . && 
-git add . && git commit -m "set me alive at github" && git push
-echo "Done - in few secounds / minutes we can see the result alive"
+ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path docs --base-href odicforcesounds.com &&
+
