117cc2eb599464531ccef0bb2dfb3a0f8baa0ae1
odicforcesounds <om@odicforcesounds.com>
2018-02-06 19:32:23 +0000

    Fix Angular project to github pages

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,2 +1,1 @@
-ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path docs --base-href odicforcesounds.com &&
-
+ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path docs --base-href "https://odicforcesounds.github.io/odicforcesounds.com/"
