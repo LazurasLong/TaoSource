4daf10ae240c13d019d555fd66b24457d8fa2c71
odicforcesounds <om@odicforcesounds.com>
2018-02-27 02:59:15 +0000

    Github Pages finaly work !

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,1 +1,3 @@
-ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
+# ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
+ng build --prod --base-href "http://odicforcesounds.github.io/odicforcesounds.com/" --output-path docs --aot --build-optimizer
+
