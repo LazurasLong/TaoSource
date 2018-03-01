7e5355d87463069fc45284b7b70b56a05f609782
odicforcesounds <om@odicforcesounds.com>
2018-03-01 19:03:32 +0000

    messing around

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,3 +1,3 @@
 # ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
-ng build --prod --base-href "http://odicforcesounds.github.io/" --output-path dist --aot --build-optimizer
+ng build --prod --base-href "http://odicforcesounds.github.io/odicforcesounds" --output-path dist --aot --build-optimizer
 
