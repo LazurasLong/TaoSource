93c1c08f79899ea4d3b9a113e811b642072b8311
odicforcesounds <om@odicforcesounds.com>
2018-03-10 19:08:19 +0000

    img trick

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,3 +1,3 @@
 # ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
-ng build --target=production --environment=prod --output-path dist --aot --build-optimizer
-
+ng build --target=production --environment=prod --output-path dist --aot --build-optimizer &&
+rm -rf dist/assets/images/
