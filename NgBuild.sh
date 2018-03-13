aea3a171cce3c5c650581ebc3b68e0f78223519b
odicforcesounds <om@odicforcesounds.com>
2018-03-13 19:18:08 +0000

    Who cares!

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,2 +1,4 @@
 # ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
-ng build --target=production --environment=prod --output-path docs/Published --aot --build-optimizer 
+ng build --target=production --environment=prod --output-path docs/Published --aot --build-optimizer &&
+rm -rf docs/Published/assets/images &&
+cp docs/Static/robots.txt docs/Static/sitemap.xml docs/Published/
