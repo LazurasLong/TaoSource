befb23f3efe5c133c2fe671cd8a878c1fa19b488
odicforcesounds <om@odicforcesounds.com>
2018-03-13 19:22:56 +0000

    a

--- NgBuild.sh
+++ NgBuild.sh
@@ -1,4 +1,4 @@
 # ng build --target=production --environment=prod --aot --build-optimizer --output-hashing --output-path dist
 ng build --target=production --environment=prod --output-path docs/Published --aot --build-optimizer &&
-rm -rf docs/Published/assets/images &&
+rm -rf docs/Published/assets/images docs/Published/3rdpartylicenses.txt &&
 cp docs/Static/robots.txt docs/Static/sitemap.xml docs/Published/
